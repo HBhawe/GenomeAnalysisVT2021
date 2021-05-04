7fbc37648377113b59305b89a2368d6ccc30ef48
HBhawe <78535979+HBhawe@users.noreply.github.com>
2021-05-04 16:35:09 +0530

    Update quality_check.sh

--- quality_check.sh
+++ quality_check.sh
@@ -3,8 +3,8 @@
 #SBATCH -A g2021012
 #SBATCH -M SNOWY
 #SBATCH -p core
-#SBATCH -n 8
-#SBATCH -t 2:00:00
+#SBATCH -n 2
+#SBATCH -t 3:00:00
 #SBATCH -J test_canu_pacbio
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user harshalb97@outlook.com
